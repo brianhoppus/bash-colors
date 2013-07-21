9e4e386763144b5c7291a225e7f9f9b123649a90
Brian Saunders <lazyrobot76@gmail.com>
2013-07-20 19:19:00 -0700

    added colors file

---
+++ colors.sh
@@ -0,0 +1,28 @@
+#!/binbash
+
+#   30-37 = foreground
+#   40-47 = background
+# colors good for dark background
+red='\033[0;31m'
+reb='\033[1;31m'
+grn='\033[0;32m'
+grb='\033[1;32m'
+ylo='\033[0;33m'
+ylb='\033[1;33m'
+blu='\033[0;34m'
+blb='\033[1;34m'
+prp='\033[0;35m'
+pnk='\033[1;35m'
+cyn='\033[0;36m'
+cyb='\033[1;36m'
+wt='\033[0;37m'
+wtb='\033[1;37m'
+gry='\033[1;30m'
+nc='\033[0m'
+
+#  add the following code for additional effects:
+#      4  =  underline
+#      5  =  blink
+#      7  =  reverse
+
+
